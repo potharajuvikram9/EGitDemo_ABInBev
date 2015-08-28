22c7625519f9af46373092532a345df8f9a46fae
Potharajuvikram9 <potharajuvikram9@gmail.com>
2015-08-28 18:04:53 +0530

    Add a second line of comment line...28/08/2015; 18:04 IST

--- EGitDemo_ABInBev/src/EGIT_Main.java
+++ EGitDemo_ABInBev/src/EGIT_Main.java
@@ -4,6 +4,8 @@ public class EGIT_Main {
 	public static void main(String[] args) {
 		System.out.println("Entered Main class");
 		System.out.println("Welcome to EGIT DEMO for sellfi Team");
+		
+		System.out.println("Adding more data for 2nd commit");
 	}
  
 }

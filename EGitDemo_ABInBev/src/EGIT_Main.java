97590c5a95bc301747eb6a1317be7085b8c78514
Potharajuvikram9 <potharajuvikram9@gmail.com>
2015-08-28 17:54:18 +0530

    Intial commit of EGitDemo_ABInBev - 28/08/2015,17:53 IST

---
+++ EGitDemo_ABInBev/src/EGIT_Main.java
@@ -0,0 +1,9 @@
+
+public class EGIT_Main {
+
+	public static void main(String[] args) {
+		System.out.println("Entered Main class");
+		System.out.println("Welcome to EGIT DEMO for sellfi Team");
+	}
+ 
+}
